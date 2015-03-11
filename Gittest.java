15c741ac1810c71f8dfdbeaa479999cdcb4ed0d0
taknil <takni@example.com>
2015-03-11 08:42:07 +0100

    use different print function

--- Gittest.java
+++ Gittest.java
@@ -4,6 +4,6 @@ public class Gittest{
   
   public static void main (String[] args){
     System.out.println("Hello AS");
-    System.out.println("good morning");
+    System.out.print("good morning");
   }
 }
